47565eccfd40366727b8b6064b77d27357224dfe
peng.xie <lanxi_322@163.com>
2017-04-10 15:50:49 +0800

    Web
    the Web of Me

---
+++ scripts/ajax.js
@@ -0,0 +1,20 @@
+
+
+
+
+function regen() {
+	// home
+	prepareSlideshow();
+	// about
+	prepareInternalnav();
+	// photos
+	preparePlaceholder();
+	prepareGallery();
+	// Schedule
+	stripeTables();
+	highlightRows();
+	displayAbbreviations();
+	// contact
+	focusLabels();
+	prepareForms();
+}
